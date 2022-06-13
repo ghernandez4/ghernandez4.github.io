df514b2bb6b5794d22f7d453c027ece8586b9936
ghernandez4 <brotherdefender@gmail.com>
2022-06-13 20:02:29 +0000

    saving sorting exercise

--- projects/asd-projects/sorting-exercise/index.js
+++ projects/asd-projects/sorting-exercise/index.js
@@ -25,10 +25,37 @@ for (i = 0; i <= array.length - 1; i++) {
 }
 }
 // TODO 3: Implement quickSort
+async function quickSort(array, left, right) {
+if ((right - left) >= 1) {
+    var index = await partition(array, left, right);
+}
+    if (left < index - 1) {
+        await quickSort(array, left, index - 1);
+    }
+    if (right > index) {
+        await quickSort(array, index, right);
+    }
 
+}
 
 // TODOs 4 & 5: Implement partition
-
+async function partition(array, left, right) {
+    var pivot = array[Math.floor((right + left)/2)].value;
+    while (left < right) {
+    while (array[left].value < pivot) {
+        left++;
+    }
+    while (array[right].value > pivot) {
+        right--;
+    }
+    if (left < right) {
+        swap(array, left, right);
+        updateCounter(quickCounter);
+        await sleep();
+    }
+}
+    return left + 1;
+}
 
 // TODO 1: Implement swap
 function swap(array, a, b) {
--- projects/asd-projects/sorting-exercise/setup.js
+++ projects/asd-projects/sorting-exercise/setup.js
@@ -6,7 +6,7 @@
 // THE CONSTANTS BELOW MAY BE ALTERED FOR EXPERIMENTATION PURPOSES
 
 // set the delay between each sort step
-const SLEEP_AMOUNT = 500;
+const SLEEP_AMOUNT = 200;
 
 // set the max number of squares
 const MAX_SQUARES = 16;
